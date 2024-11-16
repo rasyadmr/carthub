2819823386aacf820b7060ae144a75a4f4abe0b2
rasyadmr <71055821+rasyadmr@users.noreply.github.com>
2024-11-16 21:06:06 +0700

    Add: Menu Fragments

--- app/src/main/java/com/dirajarasyad/carthub/fragment/CartFragment.java
+++ app/src/main/java/com/dirajarasyad/carthub/fragment/CartFragment.java
@@ -3,18 +3,34 @@ package com.dirajarasyad.carthub.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.dirajarasyad.carthub.R;
 
 public class CartFragment extends Fragment {
+    TextView cartTitleTV;
+    RecyclerView cartItemRV;
+    Button cartCheckoutBtn;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false);
+        View view = inflater.inflate(R.layout.fragment_cart, container, false);
+
+        this.initial(view);
+
+        return view;
+    }
+
+    private void initial(View view) {
+        cartTitleTV = view.findViewById(R.id.cartTitleTV);
+        cartItemRV = view.findViewById(R.id.cartItemRV);
+        cartCheckoutBtn = view.findViewById(R.id.cartCheckoutBtn);
     }
 }
--- app/src/main/java/com/dirajarasyad/carthub/fragment/ProfileFragment.java
+++ app/src/main/java/com/dirajarasyad/carthub/fragment/ProfileFragment.java
@@ -1,5 +1,8 @@
 package com.dirajarasyad.carthub.fragment;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +10,71 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.dirajarasyad.carthub.MainActivity;
 import com.dirajarasyad.carthub.R;
+import com.dirajarasyad.carthub.auth.SessionManager;
+import com.dirajarasyad.carthub.model.User;
 
 public class ProfileFragment extends Fragment {
+    TextView profileTitleTV, profileUsernameTV, profileUsernameShowTV, profileEmailTV, profileEmailShowTV
+            , profilePhoneTV, profilePhoneShowTV, profileAddressTV, profileAddressShowTV;
+    Button profileEditBtn, profileLogoutBtn;
+    SessionManager sessionManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view =  inflater.inflate(R.layout.fragment_profile, container, false);
+
+        this.initial(view);
+        this.applyData();
+
+        profileEditBtn.setOnClickListener(this::onClick);
+        profileLogoutBtn.setOnClickListener(this::onClick);
+
+        return view;
     }
 
+    private void initial(View view) {
+        profileTitleTV = view.findViewById(R.id.profileTitleTV);
+
+        profileUsernameTV = view.findViewById(R.id.profileUsernameTV);
+        profileEmailTV = view.findViewById(R.id.profileEmailTV);
+        profilePhoneTV = view.findViewById(R.id.profilePhoneTV);
+        profileAddressTV = view.findViewById(R.id.profileAddressTV);
+
+        profileUsernameShowTV = view.findViewById(R.id.profileUsernameShowTV);
+        profileEmailShowTV = view.findViewById(R.id.profileEmailShowTV);
+        profilePhoneShowTV = view.findViewById(R.id.profilePhoneShowTV);
+        profileAddressShowTV = view.findViewById(R.id.profileAddressShowTV);
+
+        profileEditBtn = view.findViewById(R.id.profileEditBtn);
+        profileLogoutBtn = view.findViewById(R.id.profileLogoutBtn);
+
+        sessionManager = new SessionManager(requireContext());
+    }
+
+    private void applyData() {
+        User user = sessionManager.getUser();
+
+        profileUsernameShowTV.setText(user.getUsername());
+        profileEmailShowTV.setText(user.getEmail());
+        profilePhoneShowTV.setText(user.getPhone());
+        profileAddressShowTV.setText(user.getAddress());
+    }
+
+    private void onClick(View view) {
+        if (view == profileEditBtn) {
+
+        } else if (view == profileLogoutBtn) {
+            sessionManager.destroySession();
+            Intent mainpage = new Intent(requireContext(), MainActivity.class);
+            startActivity(mainpage);
+            requireActivity().finishAffinity();
+        }
+    }
 }
