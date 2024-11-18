b2e5a14f234fcaeb64f9e0740699a7892f11eaed
rasyadmr <71055821+rasyadmr@users.noreply.github.com>
2024-11-18 21:35:18 +0700

    Hotfix: Image Bitmap

--- app/src/main/java/com/dirajarasyad/carthub/utilities/Image.java
+++ app/src/main/java/com/dirajarasyad/carthub/utilities/Image.java
@@ -3,8 +3,10 @@ package com.dirajarasyad.carthub.utilities;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
 
 import java.io.ByteArrayOutputStream;
 
@@ -29,10 +31,23 @@ public class Image {
     }
 
     public byte[] getByteArray() {
-        Bitmap bitmap = ((BitmapDrawable) image).getBitmap();
+        Bitmap bitmap = this.getBitmap(image);
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);  // PNG format
 
         return byteArrayOutputStream.toByteArray();
+    }
+
+    private Bitmap getBitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        } else if (drawable instanceof VectorDrawable) {
+            Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+            drawable.setBounds(0, 0, bitmap.getWidth(), bitmap.getHeight());
+            drawable.draw(new Canvas(bitmap));
+            return bitmap;
+        } else {
+            throw new IllegalArgumentException("Unsupported drawable type");
+        }
     }
 }
