b050e95ee5ab5e7eb6f0131d27ed8d3c5c96bbf1
rasyadmr <71055821+rasyadmr@users.noreply.github.com>
2024-11-21 21:20:16 +0700

    Add: Cart Adapter & Fix Strings

---
+++ app/src/main/java/com/dirajarasyad/carthub/adapter/CartAdapter.java
@@ -0,0 +1,72 @@
+package com.dirajarasyad.carthub.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.dirajarasyad.carthub.R;
+import com.dirajarasyad.carthub.database.manager.DBCartManager;
+import com.dirajarasyad.carthub.holder.CartHolder;
+import com.dirajarasyad.carthub.model.Cart;
+
+import java.util.List;
+
+public class CartAdapter extends RecyclerView.Adapter<CartHolder> {
+    private Context context;
+    private final List<Cart> cartList;
+    private OnCartEmptyListener onCartEmptyListener;
+
+    public CartAdapter(List<Cart> cartList, OnCartEmptyListener onCartEmptyListener) {
+        this.cartList = cartList;
+        this.onCartEmptyListener = onCartEmptyListener;
+    }
+
+    public interface OnCartEmptyListener {
+        void onCartEmpty(boolean isEmpty);
+    }
+
+    @NonNull
+    @Override
+    public CartHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.holder_cart, parent, false);
+
+        return new CartHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CartHolder holder, int position) {
+        if (position >= this.getItemCount()) {
+            return;
+        }
+
+        String name = cartList.get(position).getItem().getName();
+        String quantity = cartList.get(position).getQuantity().toString();
+        @SuppressLint("DefaultLocale") String price = String.format("Rp %d", cartList.get(position).getItem().getPrice() * cartList.get(position).getQuantity());
+
+        holder.cartNameTV.setText(name);
+        holder.cartQuantityTV.setText(quantity);
+        holder.cartPriceTV.setText(price);
+        holder.cartDeleteIV.setOnClickListener(view -> {
+            DBCartManager cartManager = new DBCartManager(context);
+            cartManager.open();
+            cartManager.deleteCart(cartList.get(position).getId());
+            cartManager.close();
+            cartList.remove(position);
+            notifyItemRemoved(position);
+
+            onCartEmptyListener.onCartEmpty(cartList.isEmpty());
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return cartList.size();
+    }
+}
--- app/src/main/java/com/dirajarasyad/carthub/fragment/CartFragment.java
+++ app/src/main/java/com/dirajarasyad/carthub/fragment/CartFragment.java
@@ -3,8 +3,10 @@ package com.dirajarasyad.carthub.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +14,14 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.dirajarasyad.carthub.R;
+import com.dirajarasyad.carthub.adapter.CartAdapter;
+import com.dirajarasyad.carthub.auth.SessionManager;
+import com.dirajarasyad.carthub.database.manager.DBCartManager;
+import com.dirajarasyad.carthub.model.Cart;
 
-public class CartFragment extends Fragment {
+import java.util.List;
+
+public class CartFragment extends Fragment implements CartAdapter.OnCartEmptyListener {
     private TextView cartTitleTV;
     private RecyclerView cartItemRV;
     private Button cartCheckoutBtn;
@@ -24,6 +32,7 @@ public class CartFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_cart, container, false);
 
         this.initial(view);
+        this.onBind();
 
         return view;
     }
@@ -32,5 +41,26 @@ public class CartFragment extends Fragment {
         cartTitleTV = view.findViewById(R.id.cartTitleTV);
         cartItemRV = view.findViewById(R.id.cartItemRV);
         cartCheckoutBtn = view.findViewById(R.id.cartCheckoutBtn);
+    }
+
+    private void onBind() {
+        SessionManager sessionManager = new SessionManager(requireContext());
+        DBCartManager cartManager = new DBCartManager(requireContext());
+        cartManager.open();
+        List<Cart> cartList = cartManager.getAllCartsByUser(sessionManager.getUser());
+        cartManager.close();
+
+        if (cartList.isEmpty()) {
+            cartCheckoutBtn.setEnabled(false);
+        }
+
+        CartAdapter cartAdapter = new CartAdapter(cartList, this);
+        cartItemRV.setLayoutManager(new LinearLayoutManager(requireContext()));
+        cartItemRV.setAdapter(cartAdapter);
+    }
+
+    @Override
+    public void onCartEmpty(boolean isEmpty) {
+        cartCheckoutBtn.setEnabled(!isEmpty);
     }
 }
