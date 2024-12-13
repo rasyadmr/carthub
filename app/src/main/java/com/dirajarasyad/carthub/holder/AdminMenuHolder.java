073feb6f5ec8bdd5fb0e3923ea64d9e62afdc876
rasyadmr <71055821+rasyadmr@users.noreply.github.com>
2024-12-13 21:46:35 +0700

    Add: Inter Font (Part 2)

--- app/src/main/java/com/dirajarasyad/carthub/holder/AdminMenuHolder.java
+++ app/src/main/java/com/dirajarasyad/carthub/holder/AdminMenuHolder.java
@@ -5,7 +5,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.dirajarasyad.carthub.R;
@@ -13,12 +13,12 @@ import com.dirajarasyad.carthub.R;
 public class AdminMenuHolder extends RecyclerView.ViewHolder {
     public ImageView admin_menuIconIV;
     public TextView admin_menuTitleTV;
-    public ConstraintLayout  admin_menuContainerCL;
+    public CardView admin_menuContainerCV;
 
     public AdminMenuHolder(@NonNull View itemView) {
         super(itemView);
         this.admin_menuIconIV = itemView.findViewById(R.id.admin_menuIconIV);
         this.admin_menuTitleTV = itemView.findViewById(R.id.admin_menuTitleTV);
-        this.admin_menuContainerCL = itemView.findViewById(R.id.admin_menuContainerCL);
+        this.admin_menuContainerCV = itemView.findViewById(R.id.admin_menuContainerCV);
     }
 }
